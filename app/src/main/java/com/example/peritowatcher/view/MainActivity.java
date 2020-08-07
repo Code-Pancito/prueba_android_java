05837b575b4e865371004390b1a4e4283c2f4846
Code Pancito <codepancito@gmail.com>
2020-08-07 15:06:12 -0400

    First Commit yeah

---
+++ app/src/main/java/com/example/peritowatcher/view/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.peritowatcher.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.peritowatcher.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //TODO HACER POJOS
+}
